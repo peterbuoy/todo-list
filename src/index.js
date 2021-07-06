50697aee6ca6632e3d37fd39fa79b6981c11ca5e
peter <peterbuoy@gmail.com>
2021-07-06 10:14:04 -0700

    tweaked todo items and projects and tested via console

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import { TodoItem, Project } from "./Logic";
 
+let item1 = new TodoItem('test', 'test description', '2021-10-10', '1', 'no details');
 console.log('hello');
-let item1 = new TodoItem('test', 'test description', '2021-10-10', 'no details');
-console.log('hello');
-console.log(item1);
+console.log(item1.getTitleAndUUID());
+let projectInbox =  new Project('Inbox');
+projectInbox.addItem(item1);
+console.log(projectInbox.printAllItems());
+
+let projectPersonal = new Project('Personal');
+projectInbox.moveItem(item1, projectPersonal);
+console.log('item1 moved from inbox to personal');
+console.log(projectPersonal.printAllItems());
+let item2 = new TodoItem('test2', 'test description2', '2021-10-11', '3', 'big details');
+projectPersonal.addItem(item2);
+console.log(projectPersonal.printAllItems());
--- src/logic.js
+++ src/logic.js
@@ -1,19 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+
 class TodoItem {
-	constructor(title, description, dueDate, priority, details, project="Inbox") {
+	constructor(title, description, dueDate, priority, details) {
+		this.UUID = uuidv4();
 		this.title = title;
 		this.description = description;
 		this.dueDate = dueDate;
 		this.priority = priority;
 		this.details = details;
-		this.project = project;
+	}
+	getTitleAndUUID() {
+		return `|title:${this.title}, UUID:${this.UUID}|`;
 	}
 }
 
 class Project {
-	itemList = [];	
+	itemMap = new Map();	
 	constructor(name) {
 		this.name = name;
 	}
+	addItem(todoItem) {
+		this.itemMap.set(todoItem.UUID, todoItem);
+		console.log('item added');
+	}
+	moveItem(todoItem, destProject) {
+		destProject.itemMap.set(todoItem.UUID, todoItem);
+		this.itemMap.delete(todoItem.UUID);
+	}
+	printAllItems() {
+		return `Project: ${this.name}, Items: ${[...this.itemMap.values()].map(items => items.getTitleAndUUID())}`;
+	}
 }
 
-export {TodoItem, Project}
+export { TodoItem, Project }
