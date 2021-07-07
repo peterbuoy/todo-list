655568532d7e7ef41be029d1cc762424260eb9e1
peter <peterbuoy@gmail.com>
2021-07-07 06:08:57 -0700

    added modal for adding a new task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import { TodoItem, Project } from "./Logic";
 import "./style.css";
 
-const DIV_NEW_PROJECT = document.getElementById('div_new_project');
+const SPAN_NEW_PROJECT = document.getElementById('span_new_project');
 const DIV_ITEM_CONTAINER = document.getElementById('div_item_container');
-
-// make new inbox, add to list, add to dom
+// Initialization
+// make inbox, add to list, add to dom
 let projectInbox =  new Project('Inbox');
 let projectSpan = document.createElement('span');
 projectSpan.textContent = projectInbox.name;
-DIV_NEW_PROJECT.before(projectSpan);
-
-// make new item, add to inbox, add to dom
+SPAN_NEW_PROJECT.before(projectSpan);
+// make item, add to inbox, add to dom
 let item1 = new TodoItem('title', 'description', '2021-10-10', 1, 'no details');
 projectInbox.addItem(item1);
 let newItem = document.createElement('div');
@@ -24,7 +23,33 @@ const newProject = () => {
 		let project = new Project(projectName);
 		let item = document.createElement('span');
 		item.textContent = project.name;
-		item.addEventListener('click', () => setCurrentProject('Personal'))
-		DIV_NEW_PROJECT.before(item);
+		SPAN_NEW_PROJECT.before(item);
 	}
-DIV_NEW_PROJECT.addEventListener('click', newProject);
+
+const modal = document.querySelector(".modal");
+const BTN_ADD_TASK = document.querySelector("#btn_add_task");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+	modal.classList.toggle("show-modal");
+}
+
+BTN_ADD_TASK.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+
+SPAN_NEW_PROJECT.addEventListener('click', newProject);
+
+const handleNewTaskCreation = (e) => {
+	let title = document.getElementById('input_title').value;
+	let description = document.getElementById('input_description').value;
+	let date = document.getElementById('input_date').value;
+	let priority = document.getElementById('input_priorities').value;
+	let details = document.getElementById('input_details').value;
+	toggleModal();
+	let item1 = new TodoItem(title, description, date, priority, details);
+	projectInbox.addItem(item1);
+	console.log(projectInbox.printAllItems());
+}
+
+const BTN_CONFIRM_TASK = document.getElementById('btn_confirm_task');
+BTN_CONFIRM_TASK.addEventListener('click', handleNewTaskCreation);
--- src/logic.js
+++ src/logic.js
@@ -3,11 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 class TodoItem {
 	constructor(title, description, dueDate, priority, details) {
 		this.UUID = uuidv4();
+		this.completed = false;
 		this.title = title;
 		this.description = description;
 		this.dueDate = dueDate;
 		this.priority = priority;
 		this.details = details;
+	}
+	setCompleted() {
+		this.completed = true
 	}
 	getTitleAndUUID() {
 		return `|title:${this.title}, UUID:${this.UUID}|`;
@@ -23,8 +27,9 @@ class Project {
 		this.itemMap.set(todoItem.UUID, todoItem);
 		console.log('item added');
 	}
-	removeItem(todoItem) {
-		this.itemMap.delete(todoItem.UUID);
+	removeItemByID(todoItemUUID) {
+		this.itemMap.delete(todoItemUUID);
+		console.log('item deleted');
 	}
 	printAllItems() {
 		return `Project: ${this.name}, Items: ${[...this.itemMap.values()].map(items => items.getTitleAndUUID())}`;
